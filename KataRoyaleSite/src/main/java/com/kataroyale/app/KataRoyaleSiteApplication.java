ba133e231e38b0f307334fe5665c167df7d5771b
Dev Colyer <dcolyer@spartaglobal.com>
2024-08-02 16:44:46 +0100

    Initial Commit

---
+++ KataRoyaleSite/src/main/java/com/kataroyale/app/KataRoyaleSiteApplication.java
@@ -0,0 +1,13 @@
+package com.kataroyale.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataRoyaleSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataRoyaleSiteApplication.class, args);
+    }
+
+}
---
+++ KataRoyaleSite/src/test/java/com/kataroyale/app/KataRoyaleSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kataroyale.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataRoyaleSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
