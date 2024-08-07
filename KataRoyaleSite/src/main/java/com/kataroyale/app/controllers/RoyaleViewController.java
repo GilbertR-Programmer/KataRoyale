50207a382c8bc8e9ec321a9b31fcb39318e87ba1
Gilbert Reid <greid@spartaglobal.com>
2024-08-07 14:16:30 +0100

    Adding logging to RoyaleViewController

--- KataRoyaleSite/src/main/java/com/kataroyale/app/controllers/RoyaleViewController.java
+++ KataRoyaleSite/src/main/java/com/kataroyale/app/controllers/RoyaleViewController.java
@@ -1,6 +1,8 @@
 package com.kataroyale.app.controllers;
 
 import com.kataroyale.app.services.RoyaleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class RoyaleViewController {
 
     private final RoyaleService service;
+    private final Logger logger = LoggerFactory.getLogger(RoyaleViewController.class);
 
     @Autowired
     public RoyaleViewController(RoyaleService service) {
@@ -22,12 +25,14 @@ public class RoyaleViewController {
     @GetMapping("/")
     public String viewRoyale(Model model) {
         //biDailyUpdate();
+        logger.info("Viewing Royale");
         model.addAttribute("competitors", service.getCompetitors().toArray());
         return "home";
     }
 
     @PostMapping("/competitors")
     public String joinBattle(@RequestParam String userName) {
+        logger.info("posting user: {}", userName);
         service.addCompetitor(userName);
         return "redirect:/";
     }
@@ -37,24 +42,28 @@ public class RoyaleViewController {
     //every ten minutes every monday to friday
     @Scheduled(cron = "*/10 * * * 1-5")
     public void timedUpdate() {
+        logger.info("performing timed update");
         service.updateCompetitors();
     }
 
     //midnight every monday to thursday
     @Scheduled(cron = "0 0 * * 1-4")
     public void dailyCut() {
+        logger.info("performing daily cut");
         service.cutFifthOfCompetitors();
     }
 
     //midnight every friday
     @Scheduled(cron = "0 0 * * 5")
     public void determineWinner() {
+        logger.info("performing determine winner");
         service.pickWinner();
     }
 
     //midnight every sunday
     @Scheduled(cron = "0 0 * * 6")
-    public void resetCompetitorsSunday() {
+    public void resetCompetitors() {
+        logger.info("performing reset competitors");
         service.resetCompetitors();
     }
 }
